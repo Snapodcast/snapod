ae4a989b211c1e0fcb51f5ea5642e52821f8ff35
HelipengTony <tony.hlp@hotmail.com>
2021-05-10 11:41:30 +0800

    fix: minor style bugs

--- src/pages/single/Forgot/recover.tsx
+++ src/pages/single/Forgot/recover.tsx
@@ -52,102 +52,93 @@ export default function Forgot() {
   };
 
   return (
-    <div className="w-full h-full theme-background drag">
-      <main className="flex align-middle items-center h-full justify-center">
-        <div className="z-10 shadow-md rounded-md w-2/5 bg-white px-8 py-10 pb-11 no-drag">
-          <div className="mb-5">
-            <h1 className="font-bold text-3xl tracking-wide">
-              <span role="img" aria-label="snapod-logo" className="mr-0.5">
-                🔐
-              </span>
-              Snapod
-            </h1>
-            <p className="text-gray-400 text-sm ml-1 mt-1">
-              通过秘钥重设账户密码
-            </p>
-          </div>
-          <div className="w-full">
-            <p className="w-full">
-              <em className="ml-1 text-xs font-medium text-gray-500 not-italic">
-                秘钥 / Recovery Code
-              </em>
-              <input
-                type="text"
-                placeholder="recovery code"
-                onChange={(e) => {
-                  setCode(e.target.value);
-                }}
-                onKeyUp={() => checkValidity(true)}
-                className="mt-1 tracking-wide focus:outline-none focus:border-gray-400 border rounded-md w-full text-sm py-1.5 px-3 text-gray-700"
-              />
-              {!validCode && (
-                <span className="text-xs text-gray-400 ml-1 mt-1">
-                  秘钥长度不匹配
-                </span>
-              )}
-            </p>
-            <p className="w-full mt-3">
-              <em className="ml-1 text-xs font-medium text-gray-500 not-italic">
-                新密码 / Password
-              </em>
-              <span className="grid grid-cols-7 gap-x-1.5 mt-1">
-                <input
-                  type={showPwd ? 'text' : 'password'}
-                  placeholder="••••••••••••"
-                  maxLength={20}
-                  minLength={6}
-                  onChange={(e) => {
-                    setPassword(e.target.value);
-                  }}
-                  onKeyUp={(e) => {
-                    checkValidity(false);
-                    if (e.key === 'Enter') {
-                      doRecover();
-                    }
-                  }}
-                  className="col-start-1 col-end-7 tracking-wide focus:outline-none focus:border-gray-400 border rounded-md w-full text-sm py-1.5 px-3 text-gray-700"
-                />
-                <button
-                  className="hover:border-gray-400 border rounded-md col-start-7 col-end-8 flex justify-center items-center align-middle"
-                  onClick={() => setShowPwd(!showPwd)}
-                  type="button"
-                >
-                  <span className="w-5 h-5 text-gray-500">
-                    {showPwd ? <Icons name="eye-off" /> : <Icons name="eye" />}
-                  </span>
-                </button>
-              </span>
-              {!validPwd && (
-                <span className="text-xs text-gray-400 ml-1 mt-1">
-                  密码长度应在 6 至 20 个字符之间
-                </span>
-              )}
-            </p>
-          </div>
-          <div className="w-full mt-8 text-center">
+    <div className="z-10 shadow-md rounded-md w-2/5 bg-white px-8 py-10 pb-11 no-drag">
+      <div className="mb-5">
+        <h1 className="font-bold text-3xl tracking-wide">
+          <span role="img" aria-label="snapod-logo" className="mr-0.5">
+            🔐
+          </span>
+          Snapod
+        </h1>
+        <p className="text-gray-400 text-sm ml-1 mt-1">通过秘钥重设账户密码</p>
+      </div>
+      <div className="w-full">
+        <p className="w-full">
+          <em className="ml-1 text-xs font-medium text-gray-500 not-italic">
+            秘钥 / Recovery Code
+          </em>
+          <input
+            type="text"
+            placeholder="recovery code"
+            onChange={(e) => {
+              setCode(e.target.value);
+            }}
+            onKeyUp={() => checkValidity(true)}
+            className="mt-1 tracking-wide focus:outline-none focus:border-gray-400 border rounded-md w-full text-sm py-1.5 px-3 text-gray-700"
+          />
+          {!validCode && (
+            <span className="text-xs text-gray-400 ml-1 mt-1">
+              秘钥长度不匹配
+            </span>
+          )}
+        </p>
+        <p className="w-full mt-3">
+          <em className="ml-1 text-xs font-medium text-gray-500 not-italic">
+            新密码 / Password
+          </em>
+          <span className="grid grid-cols-7 gap-x-1.5 mt-1">
+            <input
+              type={showPwd ? 'text' : 'password'}
+              placeholder="••••••••••••"
+              maxLength={20}
+              minLength={6}
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
+              onKeyUp={(e) => {
+                checkValidity(false);
+                if (e.key === 'Enter') {
+                  doRecover();
+                }
+              }}
+              className="col-start-1 col-end-7 tracking-wide focus:outline-none focus:border-gray-400 border rounded-md w-full text-sm py-1.5 px-3 text-gray-700"
+            />
             <button
-              onClick={() => doRecover()}
-              type="submit"
-              aria-label="login"
-              className="flex justify-center align-middle items-center mb-3 text-white text-sm hover:bg-gray-700 bg-gray-600 focus:outline-none rounded-md shadow-md w-full py-2 text-center"
+              className="hover:border-gray-400 border rounded-md col-start-7 col-end-8 flex justify-center items-center align-middle"
+              onClick={() => setShowPwd(!showPwd)}
+              type="button"
             >
-              {forgotLoading ? (
-                <span className="h-5 w-5 animate-spin duration-200">
-                  <Icons name="clock" />
-                </span>
-              ) : (
-                '提交 / Submit'
-              )}
+              <span className="w-5 h-5 text-gray-500">
+                {showPwd ? <Icons name="eye-off" /> : <Icons name="eye" />}
+              </span>
             </button>
-            <Link
-              to="/landing/forgot/request"
-              className="text-sm text-gray-400"
-            >
-              申请秘钥 / Request Code
-            </Link>
-          </div>
-        </div>
-      </main>
+          </span>
+          {!validPwd && (
+            <span className="text-xs text-gray-400 ml-1 mt-1">
+              密码长度应在 6 至 20 个字符之间
+            </span>
+          )}
+        </p>
+      </div>
+      <div className="w-full mt-8 text-center">
+        <button
+          onClick={() => doRecover()}
+          type="submit"
+          aria-label="login"
+          className="flex justify-center align-middle items-center mb-3 text-white text-sm hover:bg-gray-700 bg-gray-600 focus:outline-none rounded-md shadow-md w-full py-2 text-center"
+        >
+          {forgotLoading ? (
+            <span className="h-5 w-5 animate-spin duration-200">
+              <Icons name="clock" />
+            </span>
+          ) : (
+            '提交 / Submit'
+          )}
+        </button>
+        <Link to="/landing/forgot/request" className="text-sm text-gray-400">
+          申请秘钥 / Request Code
+        </Link>
+      </div>
     </div>
   );
 }
