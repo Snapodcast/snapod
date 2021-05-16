8d9165194a500b9f5caa4d1fb65c92d8d006e8d5
HelipengTony <tony.hlp@hotmail.com>
2021-05-16 17:45:53 +0800

    fix: server url

--- src/configs/index.ts
+++ src/configs/index.ts
@@ -1,8 +1,22 @@
-export default {
-  backend_url: 'http://localhost:3333/v0/api',
-  qiniu_ak: '4mGogia1PY-PXaYvct65vITq9PeZtZXa1qxE5Ce8',
-  qiniu_sk: 'J-NECV03NfUfVgrdIfA1jkSoqMf6PS5XauY-BcxN',
-  qiniu_url: 'https://storage.snapaper.com',
-  stats_url: 'http://analytics.snapodcast.com',
-  site_url: 'https://podcast.snapodcast.com',
+const getConfigs = () => {
+  if (process.env.NODE_ENV === 'production') {
+    return {
+      backend_url: 'https://api.snapodcast.com/v0/api',
+      qiniu_ak: '4mGogia1PY-PXaYvct65vITq9PeZtZXa1qxE5Ce8',
+      qiniu_sk: 'J-NECV03NfUfVgrdIfA1jkSoqMf6PS5XauY-BcxN',
+      qiniu_url: 'https://storage.snapaper.com',
+      stats_url: 'http://analytics.snapodcast.com',
+      site_url: 'https://podcast.snapodcast.com',
+    };
+  }
+  return {
+    backend_url: 'http://localhost:3333/v0/api',
+    qiniu_ak: '4mGogia1PY-PXaYvct65vITq9PeZtZXa1qxE5Ce8',
+    qiniu_sk: 'J-NECV03NfUfVgrdIfA1jkSoqMf6PS5XauY-BcxN',
+    qiniu_url: 'https://storage.snapaper.com',
+    stats_url: 'http://analytics.snapodcast.com',
+    site_url: 'https://podcast.snapodcast.com',
+  };
 };
+
+export default getConfigs();
